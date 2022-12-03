dfdca3c5f1b5c49f919332a659c6434e4448d775
Wai <waihfun@gmail.com>
2022-12-03 02:02:19 +0000

    Database_create updated

--- Database/database_create.py
+++ Database/database_create.py
@@ -1,11 +1,15 @@
+import uuid
+import json
 from azure.cosmos import CosmosClient, PartitionKey
 
 import config
 
-databaseName = str('waihfun')
+databaseId = str('waihfun')
 # Defines a partition key for container. Can just by "/id".
 partitionKey = PartitionKey(path='/pageId')
-containerName = str('pagevisists')
+containerId = str('pagevisits')
+
+pages = ['index', 'projects']
 
 # Creates a new client instance with the variables stored in config.py
 client = CosmosClient(
@@ -13,7 +17,29 @@ client = CosmosClient(
     credential=config.settings['master_key'])
 
 # Creates a new database if it does not already exist and returns the databaseproxy reference.
-database = client.create_database_if_not_exists(id=databaseName)
+database = client.create_database_if_not_exists(id=databaseId)
 
 # Creates a new container if it does not already exist abd returns the containerproxy reference.
-container = database.create_container_if_not_exists(id=containerName, partition_key=partitionKey)
+container = database.create_container_if_not_exists(id=containerId, partition_key=partitionKey)
+
+# creates the items for each page in the pages array.
+for p in pages:
+    # Query's the container for matching pageId.
+    result = container.query_items(
+        query="SELECT * FROM " + containerId + " c WHERE c.pageId = @pageId", 
+        parameters=[{"name": "@pageId", "value": p}],
+        enable_cross_partition_query=False)
+
+    # If there are items returned then display the result else create the item.
+    if any(result):
+        # PROBLEM: Iterating through a iterator consumes it. This does nothing as it's empty after the If statement.
+        for i in result:
+            print(json.dumps(i, indent=False))
+    else:
+        print('Item ' + p + ' not found. Creating item.')
+        # Creates the item.
+        container.create_item({
+            'id': str(uuid.uuid4()),
+            'pageId': p,
+            'visits': 0
+        })
