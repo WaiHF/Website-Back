241f22611bb997484af038a5bdbe484ece20c85e
Wai <waihfun@gmail.com>
2023-01-19 23:46:30 +0000

    Implemented basic try catches

--- Azure Functions/SubmitMsg/__init__.py
+++ Azure Functions/SubmitMsg/__init__.py
@@ -3,26 +3,36 @@ import azure.functions as func
 
 def main(req: func.HttpRequest, sendGridMessage: func.Out[str]) -> func.HttpResponse:
 
+    print(req.form)
+
     # Gets the data from form.
-    name = req.form['name']
-    email = req.form['email']
-    msg = req.form['msg']
+    try:
+      name = req.form['name']
+      email = req.form['email']
+      msg = req.form['msg']
+    except:
+      print('Missing form attribute.')
+      return func.HttpResponse(f"Missing form attribute")
 
     # Creation of email.
-    message = {
-        "personalizations": [{
-          "to": [{
-            "email": "waihfun@gmail.com"}],
-            "cc": [{
-            "email": email}]
-            }],
-        "subject": "Message from: " + name,
-        "content": [{
-            "type": "text/plain",
-            "value": msg }]
-            }
-
-    # Sends email
-    sendGridMessage.set(json.dumps(message))
+    try:
+      message = {
+          "personalizations": [{
+            "to": [{
+              "email": "waihfun@gmail.com"}],
+              "cc": [{
+              "email": email}]
+              }],
+          "subject": "Message from: " + name,
+          "content": [{
+              "type": "text/plain",
+              "value": msg }]
+              }
+    
+      # Sends email
+      sendGridMessage.set(json.dumps(message))
+    except:
+      print('Failed to create and send email')
+      return func.HttpResponse(f"Failed to create and send email")
 
     return func.HttpResponse(f"Sent")
